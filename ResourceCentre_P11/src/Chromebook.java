ca7515f2cf40edd2e2339f5c510f076c60c082d5
19009811 <19009811@Jerald-Laptop>
2020-08-04 15:56:34 +0800

    update

--- ResourceCentre_P11/src/Chromebook.java
+++ ResourceCentre_P11/src/Chromebook.java
@@ -13,7 +13,6 @@ public class Chromebook extends Item {
 
 	public String toString() {
 				// Jerald
-		String output = "";
 		String info = super.toString();
 		return String.format("%-63s %-20s", info, getOs());
 	}
